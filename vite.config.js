436d2b3f91c09308920ee3ebdf5b8a537f472330
middle-aged-Weiwei <chinweihsu83221@gmail.com>
2026-01-05 00:24:27 +0800

    修正專案名稱

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  //開發中產品路徑
+  base: process.env.NODE_ENV === 'production' ? '/hex-react-class/' : '/',
   plugins: [react()],
 })
